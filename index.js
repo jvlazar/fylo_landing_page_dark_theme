6513fa6a88a6e6d881e327c8c1c1879b5c4c2b42
jvlazar <jvlazar@gmail.com>
2023-06-13 13:09:31 -0600

    improve email checking

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-function validateEmail(){
+function validateEmail() {
     let email = document.getElementById(`email_input`).value;
     let message = `Please enter a valid email addresss`;
-    if (email.includes("@")){
-        let emailArray = email.split("@");
-        if (emailArray[1].includes(".")){
-            document.getElementById("error_message").style.visibility = "hidden";
-        } else {
-           
-            document.getElementById("error_message").style.visibility = "visible";
+    if (email == "") {
+        document.getElementById("error_message").style.visibility = "visible";
+    } else {
+        if (email.includes("@")) {
+            let emailArray = email.split("@");
+            if (emailArray[1].includes(".")) {
+                emailArray = emailArray[1].split(".");
+                if (emailArray.length == 2 && emailArray[1] != "") {
+                    document.getElementById("error_message").style.visibility = "hidden";
+                } else {
+                    document.getElementById("error_message").style.visibility = "visible";
+                }
+            } else {
+
+                document.getElementById("error_message").style.visibility = "visible";
+            }
         }
     }
-        
-    
-    
+
+
+
 }
